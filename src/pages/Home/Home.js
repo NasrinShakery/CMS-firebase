185c66b77001a470a1398275d2e354e827c1e34a
NasrinShakery <nasrin.shakery8@gmail.com>
2023-11-15 18:08:25 +0330

    Feature

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Feature from '../../components/Features/Feature'
+import Chart from '../../components/Chart/Chart'
+// import  {xAxisDate} from "../../data"
+
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      {/* {console.log(xAxisDate)} */}
+      <Feature/>
+      <Chart grid title="Month Sale"  dataKey="Sale" />
+    </div>
   )
 }
 
