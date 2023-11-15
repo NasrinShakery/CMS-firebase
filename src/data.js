3550e7d225c88baa74286fcee58c85c5ab2dde1b
NasrinShakery <nasrin.shakery8@gmail.com>
2023-11-15 18:08:45 +0330

    Chart

---
+++ src/data.js
@@ -0,0 +1,49 @@
+let xAxisDate = [
+   {
+      "name" : 'Jan',
+      "Sale" : 54_000
+   },
+   {
+      "name" : 'Feb',
+      "Sale" : 11_000
+   },
+   {
+      "name" : 'Mar',
+      "Sale" : 85_000
+   },
+   {
+      "name" : 'Apr',
+      "Sale" : 43_000
+   },
+   {
+      "name" : 'May',
+      "Sale" : 32_000
+   },
+   {
+      "name" : 'Jun',
+      "Sale" : 68_000
+   },
+   {
+      "name" : 'Jul',
+      "Sale" : 90_000
+   },
+   {
+      "name" : 'Agu',
+      "Sale" : 76_000
+   },
+   {
+      "name" : 'Sep',
+      "Sale" : 111_000
+   },{
+      "name" : 'Oct',
+      "Sale" : 99_000
+   },{
+      "name" : 'Nov',
+      "Sale" : 87_000
+   },
+   {
+      "name" : 'Des',
+      "Sale" : 122_000
+   },
+]
+export {xAxisDate}
