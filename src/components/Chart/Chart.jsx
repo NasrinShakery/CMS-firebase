b4c7d586206940d444d4a4aaa4b006e5736b9a8b
NasrinShakery <nasrin.shakery8@gmail.com>
2023-11-16 13:02:19 +0330

    fix: chart style

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,51 +1,10 @@
-// import React from "react";
-// import {
-//    ResponsiveContainer,
-//    LineChart,
-//    Line,
-//    XAxis,
-//    GratesianGrid,
-//    Tooltip,
-//    CartesianGrid,
-// } from "recharts";
-
-// const Chart = ({ title, data, datakey, grid }) => {
-//    return (
-//       <div className="chart">
-//          <h3 className="chartTitle">{title}</h3>
-//          {/* <ResponsiveContainer width={"100%"} aspect={6} />
-//       <LineChart data={data} >
-//          <XAxis dataKey={"name"} stroke='#eddada'/>
-//          <Line dataKey={datakey} stroke='#eddada'/>
-//          <Tooltip/>
-//          {grid && <CartesianGrid stroke={"#9a8802"} strokeDasharray={"5 5"}/> }
-//       </LineChart> */}
-//          <LineChart
-//             width={600}
-//             height={300}
-//             data={data}
-//             margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-//          >
-//             <Line type="monotone" dataKey={datakey} stroke="#8884d8" />
-//             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-//             <XAxis dataKey="name" />
-//             {/* <YAxis /> */}
-//          </LineChart>
-//       </div>
-//    );
-// };
-
-// export default Chart;
-
-
-
-
 import React from 'react';
 import {
     ResponsiveContainer,
     LineChart,
     Line,
     XAxis,
+    YAxis,
     CartesianGrid,
     Tooltip
 } from 'recharts'
@@ -57,12 +16,13 @@ export default function Chart({title, dataKey, grid}) {
       <div className="chart">
          {console.log(xAxisDate)}
           <h3 className='chartTitle'>{title}</h3>
-          <ResponsiveContainer width="100%" aspect={4}>
+          <ResponsiveContainer width="100%" aspect={5}>
               <LineChart data={xAxisDate}>
                   <XAxis dataKey="name" stroke='#ccc' />
+                  <YAxis dataKey="Sale" />
                   <Line type="monotone" dataKey={"Sale"} stroke='#9a8802' />
                   <Tooltip />
-                  {grid && <CartesianGrid stroke='#eddada' strokeDasharray="10" />}
+                  {grid && <CartesianGrid stroke='#eddada' strokeDasharray="6" />}
               </LineChart>
           </ResponsiveContainer>
       </div>
