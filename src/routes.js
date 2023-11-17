e0972a2bc56a18646b7bedc982fe8ffee3465285
NasrinShakery <nasrin.shakery8@gmail.com>
2023-11-18 00:55:33 +0330

    product/id page

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import Home from "./pages/Home/Home";
 import UserList from "./pages/Users/UserList";
 import NewUser from "./pages/NewUser/NewUser";
 import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
 
 const routes = [
    { path: "/", element: <Home /> },
    { path: "/userlist", element: <UserList /> },
    { path: "/newUser", element: <NewUser /> },
    { path: "/products", element: <Products /> },
+   { path: "/product/:ProductID", element: <Product/> },
 ];
 export default routes;
